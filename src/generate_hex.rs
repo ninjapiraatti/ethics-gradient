cfce75edec7b47cd07e9ed9d58744f3ce4a701ac
Ninjapiraatti <tuoppil@gmail.com>
2020-11-16 18:20:52 +0200

    Added array2d crate

--- src/generate_hex.rs
+++ src/generate_hex.rs
@@ -1,3 +1,10 @@
-pub fn generate_hex(n: u32) -> String {
-    "henlo".to_string()
+use array2d::Array2D;
+
+pub fn generate_hex(n: u32) -> Array2D {
+    //const X: usize = 4;
+    //const Y: usize = 16;
+
+    let rows = vec![vec![1, 2, 3], vec![4, 5, 6]];
+    let from_rows = Array2D::from_rows(&rows);
+    from_rows
 }
